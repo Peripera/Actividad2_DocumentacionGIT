fb40c29cca7e2c443942e146adf20d46d39e5e1b
Ariana Villalobos <156480421+Peripera@users.noreply.github.com>
2025-05-06 23:21:43 -0600

    Fix: Remove unused imports and set comments properly

--- imagen.py
+++ imagen.py
@@ -3,14 +3,26 @@ import cv2
 import matplotlib.pyplot as plt
 import easyocr
 
-placa=['placa_q.jpg','placa_2.jpg','placa_3.jpg','placa_4.jpg']
+placa=['placa_q.jpg',
+       'placa_2.jpg',
+       'placa_3.jpg',
+       'placa_4.jpg']
+
 reader = easyocr.Reader(['en'], gpu=True)
 
 def procesar_imagenes(lista):
-    img = cv2.imread(lista)#Leer la imagen
-    filtro = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #Convertir la imagen a escalas de grises
-    bfiltro = cv2.bilateralFilter(filtro, 11, 17, 17)#Añadir un filtro bilateral para reducir el ruido, respetando los bordes
-    bordes = cv2.Canny(bfiltro, 30, 200)#Se aplica el algoritmo de deteccion Canny
+
+    #Leer la imagen
+    img = cv2.imread(lista)
+
+    #Convertir la imagen a escalas de grises
+    filtro = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+
+    #Añadir un filtro bilateral para reducir el ruido, respetando los bordes
+    bfiltro = cv2.bilateralFilter(filtro, 11, 17, 17)
+
+    #Se aplica el algoritmo de deteccion Canny
+    bordes = cv2.Canny(bfiltro, 30, 200)
     return img, bordes
 cont=1
 for lista in placa:
@@ -26,7 +38,6 @@ for lista in placa:
     resultado = reader.readtext(img_original)
 
     print(f"\Texto de placa: {cont}:")
-
     for _, texto, _ in resultado:
       print("-", texto)
-
+    cont +=1
